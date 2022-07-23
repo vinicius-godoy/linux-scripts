6f816777e62307b35021fc3b7fd3792000b89cce
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-07-23 18:13:40 -0300

    feat: add debug, help and verbose options to the script and improve argument handling

--- Work Scripts/convertFrontMatter.sh
+++ Work Scripts/convertFrontMatter.sh
@@ -3,13 +3,15 @@
 # Author: Vinícius Godoy (@vinicius-godoy)
 
 Main () {
+  useFlags $*
   checkArguments $*
+  setGlobalParams $*
 
-  if [ -d $1 ] && [ -d $2 ]; then
-    cd $1
+  if [ -d $oldFrontMatters ] && [ -d $newFrontMatters ]; then
+    cd $oldFrontMatters
     files=`ls *.md`
     fileCount=1
-    fileTotal=`echo "$files" | wc -w` # Gets the total number of files on the to be updated directory
+    fileTotal=`echo "$files" | wc -w | xargs` # Gets the total number of files on the to be updated directory and trim the whitespaces
     for file in $files; do
       count=0
       title=""
@@ -48,29 +50,94 @@ Main () {
       createNewFiles $*
       porcent=`bc <<< "scale=2; ($fileCount/$fileTotal)*100"`
       porcent=${porcent/.00}
-      echo "Arquivo $fileCount/$fileTotal criado com sucesso! $porcent% concluído"
+      if [ "$isVerbose" = true ]; then
+        echo -ne "Arquivo $fileCount/$fileTotal criado com sucesso! $porcent% concluído\r"
+      fi
       fileCount=$[fileCount + 1]
     done
+    if [ "$isDebug" = true ]; then
+      rm -rf $newFrontMatters/*
+    fi
+    echo
   fi
 }
 
-checkArguments (){
-  if [ $# -ne 2 ]; then
-    echo "O script precisa de 2 argumentos"
-    echo "Primeiro o diretório onde estão os frontmatters antigos e depois o novo diretório";
-    echo "Exemplo: ./script  ./novosArquivos/"
-    exit
+Help() {
+  echo "Convert all the md files from a folder from a specific frontmatter to a new layout and place them on another folder."
+  echo
+  echo "Syntax: $0 [-d] [-h] [-v] path/to/the/old-files ./new-files"
+  echo "options:"
+  echo "d     Debug mode."
+  echo "h     Print this help."
+  echo "v     Verbose mode."
+  echo
+}
+
+useFlags () {
+  options=0
+  while getopts ':dhv' OPTION; do
+    options=1
+    case "$OPTION" in
+      d) isDebug=true ;;
+      h)
+        Help
+        exit
+        ;;
+      v) isVerbose=true ;;
+      \?)
+        echo "invalid option -$OPTARG" >&2
+        echo "script usage: $0 [-d] [-h] [-v]" >&2
+        exit 1
+        ;;
+    esac
+  done
+  shift "$(($OPTIND -1))"
+}
+
+checkArguments () {
+  argumentsNum=$[2 + options]
+  firstArg=$[$# - 1]
+  secondArg=$[$#]
+
+  if [ $# -lt $argumentsNum ]; then
+    echo "The script needs 2 arguments"
+    echo "First the directory where the old frontmatters files are located and second the new empty directory where the new frontmatters will be placed.";
+    echo "Eg: $0 path/to/the/old-files ./new-files"
+    exit 1
+  fi
+
+  if [ ! -d ${!firstArg} ]; then
+    echo "The first directory provided '${!firstArg}' doesn't exist." >&2
+    echo "Run the script again with a valid directory!" >&2
+    echo
+    exit 1
+  fi
+
+  if [ ! -d ${!secondArg} ]; then
+    echo "The first directory provided '${!secondArg}' doesn't exist." >&2
+    echo "Run the script again with a valid directory!" >&2
+    echo
+    exit 1
   fi
 }
 
-descriptionNullCheck (){
+setGlobalParams () {
+  initialPwd=`pwd`
+  cd "${!firstArg}"
+  oldFrontMatters=`pwd`
+  cd "$initialPwd"
+  cd "${!secondArg}"
+  newFrontMatters=`pwd`
+}
+
+descriptionNullCheck () {
   lastValue="description"
   if [ "$value" != "''" ] & [ "$value" != "" ]; then
     description=$value
   fi
 }
 
-multiLineValues (){
+multiLineValues () {
   if [ "$key" = "---" ] || [[ ! -z $value ]]; then
     lastValue=""
     return
@@ -100,26 +167,26 @@ normalizeStrings () {
 
 createNewFiles () {
   folder=${file//.md}
-  mkdir "$2/$folder"
-  touch "$2/$folder/index.mdx"
-  echo "---" >> "$2/$folder/index.mdx"
-  echo "title:$title" >> "$2/$folder/index.mdx"
-  echo "slug:$slug" >> "$2/$folder/index.mdx"
-  echo "date:$dateVar" >> "$2/$folder/index.mdx"
-  echo "draft:$draft" >> "$2/$folder/index.mdx"
-  echo "description:$description" >> "$2/$folder/index.mdx"
-  echo "author:$author" >> "$2/$folder/index.mdx"
-  echo "category: $categories" >> "$2/$folder/index.mdx"
-  echo "tags: $tags" >> "$2/$folder/index.mdx"
-  echo "thumbnail:$thumbnail" >> "$2/$folder/index.mdx"
-  echo "---" >> "$2/$folder/index.mdx"
-  echo "" >> "$2/$folder/index.mdx"
+  mkdir "$newFrontMatters/$folder"
+  touch "$newFrontMatters/$folder/index.mdx"
+  echo "---" >> "$newFrontMatters/$folder/index.mdx"
+  echo "title:$title" >> "$newFrontMatters/$folder/index.mdx"
+  echo "slug:$slug" >> "$newFrontMatters/$folder/index.mdx"
+  echo "date:$dateVar" >> "$newFrontMatters/$folder/index.mdx"
+  echo "draft:$draft" >> "$newFrontMatters/$folder/index.mdx"
+  echo "description:$description" >> "$newFrontMatters/$folder/index.mdx"
+  echo "author:$author" >> "$newFrontMatters/$folder/index.mdx"
+  echo "category: $categories" >> "$newFrontMatters/$folder/index.mdx"
+  echo "tags: $tags" >> "$newFrontMatters/$folder/index.mdx"
+  echo "thumbnail:$thumbnail" >> "$newFrontMatters/$folder/index.mdx"
+  echo "---" >> "$newFrontMatters/$folder/index.mdx"
+  echo "" >> "$newFrontMatters/$folder/index.mdx"
   count=0
   # -n $line reads the last line of a file with no newline at the end of it
   # better description of the problem: https://stackoverflow.com/questions/12916352/shell-script-read-missing-last-line
   while IFS=: read -r line || [ -n "$line" ]; do
     if [ $count -ge 2 ]; then
-      echo "$line" >> "$2/$folder/index.mdx"
+      echo "$line" >> "$newFrontMatters/$folder/index.mdx"
     fi
     if [ "$line" = "---" ]; then
       count=$[count + 1]
