d0065b55f7acb5fb4e0e4b758c562c068dbca5fc
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-07-25 12:10:03 -0300

    feat: improve used images script to not count repeated images and add options

--- Work Scripts/usedImages.sh
+++ Work Scripts/usedImages.sh
@@ -3,32 +3,104 @@
 # Author: Vinícius Godoy (vinicius-godoy)
 
 Main () {
+  useFlags $*
   checkArguments $*
+  setGlobalParams $*
   
-  if [ -d $1 ] && [ -d $2 ]; then
-    cd $1
-    folders=`ls`
-    imageTotal=`ls $2`
-    imageTotal=`echo "$imageTotal" | wc -l`
-    imageCount=0
-    for folder in $folders; do
-      images=`grep -Eo 'https:\/\/guiamake\.s3\.sa-east-1\.amazonaws\.com\/public\/.+\.(png|jpg|jpeg)' $1/$folder/index.mdx`
-      
-      for image in $images; do
+  imagesArr=()
+  cd $postsFolder
+  folders=`ls`
+  foldersTotal=`ls | wc -l`
+  imageTotal=`ls $imagesFolder`
+  imageTotal=`echo "$imageTotal" | wc -l | xargs`
+  imageCount=0
+  progress=0
+  
+  for folder in $folders; do
+    porcent=`bc <<< "scale=2; ($progress/$foldersTotal)*100"`
+    porcent=${porcent/.00}
+    echo -ne "counting... | $porcent% concluded\r"
+    
+    images=`grep -Eo 'https:\/\/guiamake\.s3\.sa-east-1\.amazonaws\.com\/public\/.+\.(png|jpg|jpeg)' $postsFolder/$folder/index.mdx`
+    
+    for image in $images; do
+      if [[ ! " ${imagesArr[*]} " =~ " ${image} " || $isRepeating = true ]]; then
+        imagesArr+=($image)
         imageCount=$[imageCount + 1]
-      done
+      fi
     done
-    echo "$imageCount de $imageTotal images utilizadas"
+    
+    progress=$[progress + 1]
+  done
+  echo "$imageCount out of $imageTotal images used"
+}
+
+Help() {
+  echo "Counts the total amount of images from the images folder that are present on the md files from the posts folder"
+  echo
+  echo "Syntax: $0 [-h] [-r] path/to/the/posts-folder ./images/"
+  echo "options:"
+  echo "h     Print this help."
+  echo "r     Allow to count repeating images to the total."
+  echo
+}
+
+useFlags () {
+  options=0
+  while getopts ':hr' OPTION; do
+    options=1
+    case "$OPTION" in
+      h)
+        Help
+        exit
+      ;;
+      r)
+        isRepeating=true
+      ;;
+      \?)
+        echo "invalid option -$OPTARG" >&2
+        echo "script usage: $0 [-h] [-r]" >&2
+        exit 1
+      ;;
+    esac
+  done
+  shift "$(($OPTIND -1))"
+}
+
+checkArguments () {
+  argumentsNum=$[2 + options]
+  firstArg=$[$# - 1]
+  secondArg=$[$#]
+  
+  if [ $# -lt $argumentsNum ]; then
+    echo "The script needs 2 arguments"
+    echo "First the directory where the blog posts are located and second the directory with the s3 bucket images.";
+    echo "Eg: $0 path/to/the/posts-folder ./images/"
+    exit 1
+  fi
+  
+  if [ ! -d ${!firstArg} ]; then
+    echo "The first directory provided '${!firstArg}' doesn't exist." >&2
+    echo "Run the script again with a valid directory!" >&2
+    echo
+    exit 1
+  fi
+  
+  if [ ! -d ${!secondArg} ]; then
+    echo "The first directory provided '${!secondArg}' doesn't exist." >&2
+    echo "Run the script again with a valid directory!" >&2
+    echo
+    exit 1
   fi
 }
 
-checkArguments (){
-  if [ $# -ne 2 ]; then
-    echo "O script precisa de 2 argumentos"
-    echo "Primeiro o diretório onde estão as pastas de posts e depois diretório com as imagens";
-    echo "Exemplo: ./script  ./novosArquivos/"
-    exit
-  fi
+setGlobalParams () {
+  initialPwd=`pwd`
+  cd "${!firstArg}"
+  postsFolder=`pwd`
+  cd "$initialPwd"
+  cd "${!secondArg}"
+  imagesFolder=`pwd`
 }
 
 Main $*
